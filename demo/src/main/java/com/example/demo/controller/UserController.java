4088a9ff4ce61e8874916be382cb78c7fc82f1a5
safiradwikhairunisa <safiradwikhairunisa@gmail.com>
2026-04-14 14:27:59 +0700

    perubahan isi

--- demo/src/main/java/com/example/demo/controller/UserController.java
+++ demo/src/main/java/com/example/demo/controller/UserController.java
@@ -1,4 +1,36 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class UserController {
+    public final String USERNAME = "admin";
+    public final String PASSWORD = "20240140173";
+
+    @GetMapping("/")
+    public String login() {
+        return  "login";
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam String username,
+                        @RequestParam String password,
+                        Model model) {
+        if (USERNAME.equals(username) && PASSWORD.equals(password)) {
+            return "home";
+        } else {
+            model.addAttribute("error",
+                    "Invalid username or password");
+            return "login";
+        }
+    }
+    @GetMapping("/home")
+    public String homepage() {
+        return  "home";
+    }
 }
+
--- demo/src/main/java/com/example/demo/model/User.java
+++ demo/src/main/java/com/example/demo/model/User.java
@@ -1,35 +1,15 @@
 package com.example.demo.model;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Controller
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
-    public final String USERNAME = "admin";
-    public final String PASSWORD = "20240140173";
-
-    @GetMapping("/")
-    public String login() {
-        return  "login";
-    }
-
-    @PostMapping("/login")
-    public String login(@RequestParam String username,
-                        @RequestParam String password,
-                        Model model) {
-        if (USERNAME.equals(username) && PASSWORD.equals(password)) {
-            return "home";
-        } else {
-            model.addAttribute("error",
-                    "Invalid username or password");
-            return "login";
-        }
-    }
-    @GetMapping("/home")
-    public String homepage() {
-        return  "home";
-    }
+    private String nama;
+    private String nim;
+    private String jenisKelamin;
+    private String email;
 }
